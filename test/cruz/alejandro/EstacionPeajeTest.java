fd10c0582a30ca6ce3ad923d7af3c0f5cf90f2e1
Alejandro Cruz <alejandrodavidcruz@gmail.com>
2021-02-03 22:07:09 -0300

    Tests

--- test/cruz/alejandro/EstacionPeajeTest.java
+++ test/cruz/alejandro/EstacionPeajeTest.java
@@ -4,15 +4,27 @@ import org.junit.Test;
 
 public class EstacionPeajeTest {
 
-	
-///////	
-	
+
 	@Test
 	public void queQueObtieneListaDeCabinasOrdenaPorImporteDescendente () {
 		
+		EstacionDePeaje e1 = new EstacionDePeaje();
+		Cabina c1 = new Cabina(2);
+		Cabina c2 = new Cabina(3);
+		Cabina c3 = new Cabina(7);
+		Cabina c4 = new Cabina(19);
+		Cabina c5 = new Cabina(10);
+		e1.agregarCabinas(c1);
+		e1.agregarCabinas(c2);
+		e1.agregarCabinas(c3);
+		e1.agregarCabinas(c4);
+		e1.agregarCabinas(c5);
+
+		Orden o1 = new Orden(3);
+		System.out.println(e1.otenerCabinasOrdenadaImporteDescendente(o1));
+		
+		
 	}
-	
-	
 	
 	
 	
--- test/cruz/alejandro/TelepaseTest.java
+++ test/cruz/alejandro/TelepaseTest.java
@@ -1,26 +1,38 @@
 package cruz.alejandro;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class TelepaseTest {
 	
 	
 	@Test
-	public void queVerificaQueSeEncuentreElTagConIdUno () {
-		//Para este test se debeinvocar al metodo public Boolean verifcarExiste(Tag tag)
-		//el mismo no se puede modificar
+	public void queVerificaQueSeEncuentreElTagConIdUno () throws TagNoEncontradoException {
+		Telepase t1 = new Telepase(1);
+		Tag t = new Tag(1);
+		assertTrue(t1.verifcarExiste(t));
 		
 	}
 	
 
-	@Test
-	public void queAlPasarAutobusconSaldoInsuficienteLanceUnaExcepcion () {
+	@Test(expected = SaldoInsuficienteError.class)
+	public void queAlPasarAutobusconSaldoInsuficienteLanceUnaExcepcion () throws VehiculoNoPermitidoExceptions, TagNoEncontradoException, SaldoInsuficienteError {
+		Tag t1 = new Tag(2);
+		AutoBus b1 = new AutoBus("23",t1);
+		Telepase te = new Telepase(1);
+		te.cargarTag(t1);
+		te.pagarAutomatico(b1);
 		
 	}
 	
-	@Test
-	public void queAlPasarUnAutobusconUnTagInvalidoLanceUnaExcepcion () {
-		
+	@Test (expected = VehiculoNoPermitidoExceptions.class)
+	public void queAlPasarUnAutobusconUnTagInvalidoLanceUnaExcepcion () throws VehiculoNoPermitidoExceptions, TagNoEncontradoException, SaldoInsuficienteError {
+		Tag t1 = new Tag(2);
+		Coche c1 = new Coche("s");
+		Telepase te = new Telepase(1);
+		te.cargarTag(t1);
+		te.pagarAutomatico(c1);
 	}
 	
 	
